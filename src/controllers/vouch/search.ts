a4fa2cd2b0df81d0a77dba7f7159b4c5286d711d
Matheus Alencar <matheus@kleros.io>
2021-04-20 12:51:08 -0300

    feat: allow searching by voucherAddr

--- src/controllers/vouch/search.ts
+++ src/controllers/vouch/search.ts
@@ -4,13 +4,25 @@ import requestMiddleware from '../../middleware/request-middleware';
 import Vouch from '../../models/Vouch';
 
 const searchVouchSchema = Joi.object().keys({
-  submissionId: Joi.string().required()
+  submissionId: Joi.string(),
+  voucherAddr: Joi.string()
 });
 
 const search: RequestHandler = async (req, res) => {
-  const { submissionId } = req.query;
+  const { submissionId, voucherAddr } = req.query;
 
-  const query = { submissionId: new RegExp(`.*${String(submissionId).toLowerCase()}.*`, 'i') };
+  const query: any = {};
+  if (submissionId) {
+    query['$expr'] = {
+      $eq: [String(submissionId).toLowerCase(), '$submissionId']
+    };
+  }
+
+  if (voucherAddr) {
+    query['$expr'] = query['$expr'] || {};
+    query['$expr']['$in'] = [voucherAddr, '$vouchers'];
+  }
+
   const vouches = await Vouch.find(query);
   res.send({ vouches });
 };
